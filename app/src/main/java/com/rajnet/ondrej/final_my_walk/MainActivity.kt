5d9298ac30896e44c0729bd128f42cbf8efba038
onrajnet <ondrej.rajnet@gmail.com>
2021-05-14 08:20:34 +0200

    step_count_v1

--- app/src/main/java/com/rajnet/ondrej/final_my_walk/MainActivity.kt
+++ app/src/main/java/com/rajnet/ondrej/final_my_walk/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.rajnet.ondrej.final_my_walk
 
 import DatabaseHandler
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private var sensorManager: SensorManager? = null
+
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +34,10 @@ class MainActivity : AppCompatActivity() {
             addRecord()
         }
 
+        loadData()
+        resetStep()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
 
     }
 
@@ -30,12 +46,12 @@ class MainActivity : AppCompatActivity() {
 
         val name = etName.text.toString()
         val date = etDateId.text.toString()
-        val walk = 500
+        val walk = totalSteps
         walk.toLong()
         val databaseHandler: DatabaseHandler = DatabaseHandler(this)
         if (!name.isEmpty() && !date.isEmpty()) {
             val status =
-                databaseHandler.addWalk(HisModelClass(0, name, date, walk))
+                databaseHandler.addWalk(HisModelClass(0, name, date, totalSteps.toInt()))
             if (status > -1) {
                 Toast.makeText(applicationContext, "Record saved", Toast.LENGTH_LONG).show()
                 etName.text.clear()
@@ -48,5 +64,60 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_LONG
             ).show()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if(stepSensor == null){
+            Toast.makeText(this, "Zařízení nemá pohybový senzor", Toast.LENGTH_SHORT).show()
+        }else{
+            sensorManager?.registerListener(this, stepSensor,SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(running){
+            totalSteps = event!!.values[0]
+            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+            tv_stepsTaken.text = ("$currentSteps")
+
+            progress_circular.apply {
+                setProgressWithAnimation(currentSteps.toFloat())
+            }
+
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun resetStep(){
+        tv_stepsTaken.setOnClickListener{
+            Toast.makeText(this, "Dlouhé podržení pro reset kroků", Toast.LENGTH_SHORT).show()
+        }
+
+        tv_stepsTaken.setOnLongClickListener {
+            previousTotalSteps = totalSteps
+            tv_stepsTaken.text = 0.toString()
+            saveData()
+            true
+        }
+    }
+
+    private fun saveData(){
+        val sharedPreferences = getSharedPreferences("Preference", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putFloat("key1", previousTotalSteps)
+        editor.apply()
+    }
+    private fun loadData(){
+        val sharedPreferences = getSharedPreferences("Preference", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1",0f)
+        Log.d("MainActivity", "$savedNumber")
+        previousTotalSteps = savedNumber
     }
 }
